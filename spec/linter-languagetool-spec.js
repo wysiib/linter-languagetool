b21b2085fb84244386a37a94e8d47fd132df1fc6
Sebastian Krings <sebastian@krin.gs>
2017-04-04 14:40:38 +0200

    adapt test case to linter v2 api

--- spec/linter-languagetool-spec.js
+++ spec/linter-languagetool-spec.js
@@ -15,15 +15,15 @@ describe('The languagetool-linter for AtomLinter', () => {
         return lint(editor).then(messages => {
           expect(messages.length).toEqual(8);
 
-          expect(messages[0].type).toBeDefined();
-          expect(messages[0].type).toEqual('Error');
-          expect(messages[0].text).toBeDefined();
-          expect(messages[0].text).toEqual('This sentence does not start with an uppercase letter');
-          expect(messages[0].filePath).toBeDefined();
-          expect(messages[0].filePath).toMatch(/.+languagetool-hp-test\.txt$/);
-          expect(messages[0].range).toBeDefined();
-          expect(messages[0].range.length).toEqual(2);
-          expect(messages[0].range).toEqual([[2, 0], [2, 2]]);
+          expect(messages[0].severity).toBeDefined();
+          expect(messages[0].severity).toEqual('error');
+          expect(messages[0].excerpt).toBeDefined();
+          expect(messages[0].excerpt).toEqual('This sentence does not start with an uppercase letter');
+          expect(messages[0].location.file).toBeDefined();
+          expect(messages[0].location.file).toMatch(/.+languagetool-hp-test\.txt$/);
+          expect(messages[0].location.position).toBeDefined();
+          expect(messages[0].location.position.length).toEqual(2);
+          expect(messages[0].location.position).toEqual([[2, 0], [2, 2]]);
         });
       });
     });
