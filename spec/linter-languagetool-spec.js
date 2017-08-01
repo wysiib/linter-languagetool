2b6592068e76d1ed60456a26640557e1b154656b
Tobias Heß <tobias.hess@tu-dresden.de>
2017-08-01 14:53:24 +0200

    Set message severity depending on the categorie id of the rule for english and german rules. Also add specs for it.

--- spec/linter-languagetool-spec.js
+++ spec/linter-languagetool-spec.js
@@ -49,4 +49,67 @@ describe('The languagetool-linter for AtomLinter', () => {
       });
     });
   });
+
+  it('checks the resulting severity for different rule categories (de)', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/test_files/languagetool-cat-test-de.txt').then(editor => {
+        return lint(editor).then(messages => {
+          expect(messages.length).toEqual(19);
+
+          expect(messages[0].severity).toBeDefined();
+          expect(messages[0].severity).toEqual('error');
+          expect(messages[1].severity).toBeDefined();
+          expect(messages[1].severity).toEqual('info');
+          expect(messages[2].severity).toBeDefined();
+          expect(messages[2].severity).toEqual('error');
+          expect(messages[3].severity).toBeDefined();
+          expect(messages[3].severity).toEqual('info');
+          expect(messages[4].severity).toBeDefined();
+          expect(messages[4].severity).toEqual('error');
+          expect(messages[5].severity).toBeDefined();
+          expect(messages[5].severity).toEqual('info');
+          expect(messages[6].severity).toBeDefined();
+          expect(messages[6].severity).toEqual('info');
+          expect(messages[7].severity).toBeDefined();
+          expect(messages[7].severity).toEqual('error');
+          expect(messages[8].severity).toBeDefined();
+          expect(messages[8].severity).toEqual('warning');
+          expect(messages[9].severity).toBeDefined();
+          expect(messages[9].severity).toEqual('info');
+          expect(messages[10].severity).toBeDefined();
+          expect(messages[10].severity).toEqual('warning');
+          expect(messages[11].severity).toBeDefined();
+          expect(messages[11].severity).toEqual('error');
+          expect(messages[12].severity).toBeDefined();
+          expect(messages[12].severity).toEqual('error');
+          expect(messages[13].severity).toBeDefined();
+          expect(messages[13].severity).toEqual('error');
+          expect(messages[14].severity).toBeDefined();
+          expect(messages[14].severity).toEqual('warning');
+          expect(messages[15].severity).toBeDefined();
+          expect(messages[15].severity).toEqual('info');
+          expect(messages[16].severity).toBeDefined();
+          expect(messages[16].severity).toEqual('warning');
+          expect(messages[17].severity).toBeDefined();
+          expect(messages[17].severity).toEqual('warning');
+          expect(messages[18].severity).toBeDefined();
+          expect(messages[18].severity).toEqual('error');
+        });
+      });
+    });
+  });
+
+  it('checks the resulting severity for different rule categories (en)', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/test_files/languagetool-cat-test-en.txt').then(editor => {
+        return lint(editor).then(messages => {
+          expect(messages.length).toEqual(2);
+          expect(messages[0].severity).toBeDefined();
+          expect(messages[0].severity).toEqual('error');
+          expect(messages[1].severity).toBeDefined();
+          expect(messages[1].severity).toEqual('info');
+        });
+      });
+    });
+  });
 });
